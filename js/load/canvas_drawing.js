44ae7a4d61fec556f6a2a08a4d0b8db1402b79bc
Brice Harismendy <briceharismendy@gmail.com>
2019-12-19 07:57:37 +0100

    ajout de la fonctionnalité dessin

---
+++ js/load/canvas_drawing.js
@@ -0,0 +1,69 @@
+var mousePressed = false;
+var lastX, lastY;
+var ctx;
+
+//resize image with off-screen canvas
+function imageToDataUri(img, width, height) {
+
+    // create an off-screen canvas
+    var canvas = document.createElement('canvas'),
+        ctx = canvas.getContext('2d');
+
+    // set its dimension to target size
+    canvas.width = width;
+    canvas.height = height;
+
+    // draw source image into the off-screen canvas:
+    ctx.drawImage(img, 0, 0, width, height);
+
+    // encode image to data-uri with base64 version of compressed image
+    return canvas.toDataURL("image/png");
+}
+
+function InitThis() {
+    ctx = document.getElementById('sheet').getContext("2d");
+
+    $('#sheet').mousedown(function (e) {
+        mousePressed = true;
+        Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
+    });
+
+    $('#sheet').mousemove(function (e) {
+        if (mousePressed) {
+            Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, true);
+        }
+    });
+
+    $('#sheet').mouseup(function (e) {
+        mousePressed = false;
+        let img = imageToDataUri(document.getElementById("sheet"),28,28)//resize it
+        let imgElement = document.getElementById("imageResult").setAttribute("src",img);// display it
+    });
+	    $('#sheet').mouseleave(function (e) {
+        mousePressed = false;
+    });
+}
+
+function Draw(x, y, isDown) {
+    if (isDown) {
+        ctx.beginPath();
+        ctx.strokeStyle = "000000";
+        ctx.lineWidth = 9;
+        ctx.lineJoin = "round";
+        ctx.moveTo(lastX, lastY);
+        ctx.lineTo(x, y);
+        ctx.closePath();
+        ctx.stroke();
+    }
+    lastX = x; lastY = y;
+}
+
+function clearArea() {
+    // Use the identity matrix while clearing the canvas
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    document.getElementById("imageResult").setAttribute("src","");
+}
+
+// init the cancas
+document.addEventListener('DOMContentLoaded', InitThis);
