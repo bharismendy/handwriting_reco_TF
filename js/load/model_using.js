9a399f27d235a4a14c8f93d761b790945bb773ac
bharismendy <brice.harismendy@ignimotion.com>
2020-01-06 15:39:44 +0100

    fix little mistake and add comment

--- js/load/model_using.js
+++ js/load/model_using.js
@@ -4,8 +4,8 @@ async function loadModel(){
     let url = window.location.href.replace(window.location.href.substring(window.location.href.lastIndexOf('/') + 1),'');
     modelJson = await tf.loadLayersModel( url + 'data/my-model.json' );
 }
-function guessIt(imageToGuess){
-  let inputTensor = tf.browser.fromPixels(document.getElementById('imageResult'), 1)
+function guessIt(){
+  let inputTensor = tf.browser.fromPixels(document.getElementById('imageResult'), 1)//get the pre-rendered image to predict in the <img> tag
     .reshape([1, 28, 28, 1])
     .cast('float32');
   let predictionResult =  modelJson.predict(inputTensor).dataSync();
